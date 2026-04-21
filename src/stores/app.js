466b532f6d37a99a3e55448fb4215a6397f05412
Usama iMac <usamaxyz@gmail.com>
2026-04-22 00:53:37 +0300

    app version

--- src/stores/app.js
+++ src/stores/app.js
@@ -3,7 +3,7 @@ import { defineStore } from 'pinia';
 export const useAppStore = defineStore('app', {
     state: () => ({
         app_name: 'Gamearound',
-        version: '1.0.1',
+        version: '1.0.2',
         permissions: ['admin', 'manage_users', 'manage_games', 'manage_currencies'],
     }),
 
